8ff0630f9c8ac607909a7b31d4ddc6a4afc486e2
Ali salhab <alisalhab258@gmail.com>
2026-01-02 16:22:01 +0300

    chore: update dependencies and add Tailwind CSS support
    - Added @clerk/clerk-expo and @tanstack/react-query to dependencies.
    - Updated react and react-dom to latest versions.
    - Updated react-native-reanimated and react-native-safe-area-context to compatible versions.
    - Added nativewind and tailwindcss for styling.
    - Removed reset-project.js script as it is no longer needed.
    - Updated tsconfig.json to include nativewind-env.d.ts.
    - Created new layout and index files for authentication routes.
    - Configured Babel and Metro for NativeWind and Tailwind CSS integration.
    - Added global CSS file for Tailwind directives.
    - Created tailwind.config.js for Tailwind CSS configuration.

---
+++ mobile/app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+
+export default function AuthRoutesLayout() {
+  const { isSignedIn } = useAuth();
+  if (isSignedIn) {
+    // If the user is signed in, redirect to the main app
+    return <Redirect href={"/"} />;
+  }
+  return <Stack />;
+}
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,24 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
