b324c25220038eac124e29063cad4bfaf0efd54a
Ali salhab <alisalhab258@gmail.com>
2026-01-03 08:56:04 +0300

    feat: add expo-auth-session and update dependencies; implement useSocialAuth hook for social authentication

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>Tab layout</Text>
+    </View>
+  );
+};
+
+export default TabsLayout;
